7d4e5e6e7c32e565bbe8a54c0165b4d89a5a9ab8
Filip Christoffersson <flpchrsn@gmail.com>
2016-05-21 17:40:44 +0200

    Add helper function to logg response time :clock1:

---
+++ logger.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func Logger(inner http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		start := time.Now()
+
+		inner.ServeHTTP(res, req)
+		log.Printf(
+			"%s\t%s\t%s\t%s",
+			req.Method,
+			req.RequestURI,
+			name,
+			time.Since(start),
+		)
+	})
+}
