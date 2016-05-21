23a49fca4e65ea1f0dcf33e2666582b9680d2869
Filip Christoffersson <flpchrsn@gmail.com>
2016-05-21 14:31:19 +0200

    Use mux as router, initialize and attach router functions. For now just returning strings

--- main.go
+++ main.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", Index)
-	http.ListenAndServe(":8080", nil)
+	// Setup the router
+	r := mux.NewRouter().StrictSlash(true)
+
+	// Attach the routes
+	r.HandleFunc("/", Index)
+	r.HandleFunc("/todos", Todos)
+	r.HandleFunc("/todos/{todoId}", TodoShow)
+
+	// Listen on port :8080
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Everything is GO")
+	fmt.Fprintln(w, "Everything is GO")
+}
+
+func Todos(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Show all todos")
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintln(w, "Show", todoId)
 }
